617f543376de0e99829e4e41da2fed6752c3a501
Ladd Hanson <ladd@austin.utexas.edu>
2017-03-01 17:47:48 -0700

    close #4

--- validate-bags.sh
+++ validate-bags.sh
@@ -1,2 +1,53 @@
-#!/bin/sh
+#!/bin/bash
 
+USAGE="Usage: $(basename $0) -i <input directory>"
+
+input=
+output=
+
+while getopts "i:" opt; do
+	case $opt in
+        i)
+                input=$OPTARG
+                ;;
+        \?)
+                echo ${USAGE}
+                exit 1
+        esac
+done
+
+if [ "${input}" == '' ]; then
+        echo ${USAGE}
+        exit 1
+fi
+
+if [ ! -d ${input} ]; then
+        echo "${input}: not a directory."
+        exit 1
+fi
+
+input=$(cd ${input}; pwd) # convert to absolute path
+
+if [ ${input: -5} != ".bags" ]; then
+        echo "${input}: not a directory ending with '.bags'."
+        exit 1
+fi
+
+# https://github.com/LibraryOfCongress/bagit-python
+
+bagit=/usr/local/bin/bagit.py
+
+if [ ! -x ${bagit} ]; then
+        echo "${bagit}: not found."
+        exit 1
+fi
+
+for i in $(ls $input); do
+	$bagit --validate $input/$i
+
+	if [ $? -ne 0 ]; then
+		echo
+		echo "Input directory must a directory of valid bags."
+		exit 1
+	fi
+done
