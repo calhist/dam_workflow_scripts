1cb655352979d54fc9726e2d060e757075ad7cfd
Ladd Hanson <ladd@austin.utexas.edu>
2017-03-02 19:57:47 -0700

    close $5

--- create-batch.sh
+++ create-batch.sh
@@ -1,61 +1,101 @@
-#!/bin/sh
+#!/bin/bash
 
-#src=/data/staging/bags
-#src=/home/ubuntu/bags
-src=/home/chsadmin/bags
-dst=/tmp/test
+USAGE="Usage: $(basename $0) -i <input directory> -c <collection name>"
 
-[ -d $dst ] && rm -rf $dst
+input=
+output=$(mktemp -d --tmpdir=/tmp $(basename $0 .sh).XXXX)
 
-mkdir /tmp/test
+function cleanup {
+	rm -rf $output
+	echo "Deleted temp working directory $output"
+}
 
-for i in $(ls $src/*/data/*.tif); do
+trap cleanup EXIT
+
+while getopts "i:c:" opt; do
+        case $opt in
+        i)
+                input=$OPTARG
+                ;;
+        c)
+                collection=$OPTARG
+                ;;
+        \?)
+                echo ${USAGE}
+                exit 1
+        esac
+done
+
+if [ "${input}" == '' ]; then
+        echo ${USAGE}
+        exit 1
+fi
+
+if [ ! -d ${input} ]; then
+        echo "${input}: not a directory."
+        exit 1
+fi
+
+input=$(cd ${input}; pwd) # convert to absolute path
+
+if [ ${input: -5} != ".bags" ]; then
+        echo "${input}: not a directory ending with '.bags'."
+        exit 1
+fi
+
+#drush=/usr/local/bin/drush
+#
+#if [ ! -x ${drush} ]; then
+#        echo "${drush}: not found."
+#        exit 1
+#fi
+
+bagit=/usr/local/bin/bagit.py
+
+if [ ! -x ${bagit} ]; then
+        echo "${bagit}: not found."
+        exit 1
+fi
+
+for i in $(ls $input); do
+        $bagit --validate $input/$i
+
+        if [ $? -ne 0 ]; then
+                echo
+                echo "Input directory must a directory of valid bags."
+                exit 1
+        fi
+done
+
+for i in $(ls $input/*/data/*|egrep '.[jpg|tif|png$]'); do
 	bag=$(basename $i .tif)
 	dir=$(dirname $i)
 	tif=$(basename $i)
 	xml=$(basename $i .tif).xml
 
-	if [ $bag = 'map_0118' ]; then
-		continue
-		cp $dir/$tif $dst/$tif
-		cp $dir/$xml $dst/$xml
+	echo cp $dir/$tif $output/$tif
+
+	if [ -f $dir/$xml ]; then
+		echo cp $dir/$xml $output/$xml
 	fi
-	if [ $bag = 'map_0389' ]; then
-		continue
-		cp $dir/$tif $dst/$tif
-		cp $dir/$xml $dst/$xml
-	fi
-	if [ $bag = 'map_0468' ]; then
-		continue
-		cp $dir/$tif $dst/$tif
-		cp $dir/$xml $dst/$xml
-	fi
-	cp $dir/$tif $dst/$tif
-	cp $dir/$xml $dst/$xml
 done
 
-#sudo drush \
-#	--root=/var/www/drupal7 \
-#	--user=admin \
-#	--uri=http://default \
-#	st
-
-sudo drush \
+echo drush \
 	--root=/var/www/drupal7 \
 	--user=admin \
 	--uri=http://default \
 	islandora_batch_scan_preprocess \
 	--content_models=islandora:sp_large_image_cmodel \
 	--namespace=islandora \
-	--parent=islandora:1473 \
+	--parent=$collection \
 	--parent_relationship_pred=isMemberOfCollection \
 	--type=directory \
 	--target=/tmp/test
 
-sudo drush \
-	--root=/var/www/drupal7 \
-	--user=admin \
-	--uri=http://default \
-	islandora_batch_ingest
+#echo drush \
+#	--root=/var/www/drupal7 \
+#	--user=admin \
+#	--uri=http://default \
+#	islandora_batch_ingest
 
 
