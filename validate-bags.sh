fa5f3d6e0d3f70a07fb343747f468fc8f589affe
Ladd Hanson <ladd@austin.utexas.edu>
2017-03-03 19:58:55 -0700

    close #6

--- validate-bags.sh
+++ validate-bags.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-USAGE="Usage: $(basename $0) -i <input directory>"
+USAGE="Usage: $(basename $0) -i <input directory> -c <days>"
 
 input=
 output=
 
-while getopts "i:" opt; do
+while getopts "i:c:" opt; do
 	case $opt in
         i)
                 input=$OPTARG
+                ;;
+        c)
+                days=$OPTARG
                 ;;
         \?)
                 echo ${USAGE}
@@ -26,12 +29,12 @@ if [ ! -d ${input} ]; then
         exit 1
 fi
 
-input=$(cd ${input}; pwd) # convert to absolute path
-
-if [ ${input: -5} != ".bags" ]; then
-        echo "${input}: not a directory ending with '.bags'."
+if [[ ! ${days} =~ ^\+[0-9]+$ ]]; then
+        echo "${days}: invalid number of days."
         exit 1
 fi
+
+input=$(cd ${input}; pwd) # convert to absolute path
 
 # https://github.com/LibraryOfCongress/bagit-python
 
@@ -42,12 +45,33 @@ if [ ! -x ${bagit} ]; then
         exit 1
 fi
 
-for i in $(ls $input); do
-	$bagit --validate $input/$i
+if [ ${input} -a ${days} ]; then
+	for a in $(find ${input} -maxdepth 1 -type d -name "*.bags" -mtime ${days}); do
+		for b in $(find ${a} -maxdepth 1 -type d ! -name "*.bags" -mtime ${days}); do
+			$bagit --validate $b
 
-	if [ $? -ne 0 ]; then
-		echo
-		echo "Input directory must a directory of valid bags."
-		exit 1
+			if [ $? -ne 0 ]; then
+				echo
+				echo "Input directory must a directory of valid bags."
+				exit 1
+			fi
+		done
+
+ 		touch -c -m -t $(date +%Y%m%d0000.00) $a
+	done
+else
+	if [ ${input: -5} != ".bags" ]; then
+	        echo "${input}: not a directory ending with '.bags'."
+	        exit 1
 	fi
-done
+
+	for i in $(ls $input); do
+		$bagit --validate $input/$i
+
+		if [ $? -ne 0 ]; then
+			echo
+			echo "Input directory must a directory of valid bags."
+			exit 1
+		fi
+	done
+fi
