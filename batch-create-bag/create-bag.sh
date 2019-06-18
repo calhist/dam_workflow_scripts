a254200d494f932c185c35469607c535501468e5
Ladd Hanson <ladd@austin.utexas.edu>
2019-06-18 16:27:20 -0500

    update

--- batch-create-bag/create-bag.sh
+++ batch-create-bag/create-bag.sh
@@ -38,43 +38,53 @@ which bagit.py   >/dev/null 2>&1 || echo_exit "bagit.py not found"
 which fits.sh    >/dev/null 2>&1 || echo_exit "fits.sh not found"
 which xmlstarlet >/dev/null 2>&1 || echo_exit "xmlstarlet not found"
 
-if [[ "${input}" =~ ^s3://([0-9]+-input)/(.+)/((.+)\.(.+))$ ]]; then
+# if [[ "${input}" =~ ^s3://([0-9]+-input)/(.+)/((.+)\.(.+))$ ]]; then
+if [[ "${input}" =~ ^s3://([0-9]+-input)/(([^./]+)(\..+)?)/((.+)(\..+))$ ]]; then
+#                         1              23    4        56   7
   src_url=${BASH_REMATCH[0]}
   src_bucket=${BASH_REMATCH[1]}
   src_prefix=${BASH_REMATCH[2]}
-  src_object=${BASH_REMATCH[3]}
-  src_base=${BASH_REMATCH[4]}
-  src_ext=${BASH_REMATCH[5]}
+  src_prefix_base=${BASH_REMATCH[3]}
+  src_prefix_ext=${BASH_REMATCH[4]}
+  src_object=${BASH_REMATCH[5]}
+  src_object_base=${BASH_REMATCH[6]}
+  src_object_ext=${BASH_REMATCH[7]}
 else
   echo_exit "${input} must be a S3 URL"
 fi
 
+echo $src_url: $src_bucket $src_prefix $src_object
+echo $src_prefix: $src_prefix_base $src_prefix_ext
+echo $src_object: $src_object_base $src_object_ext
+echo 
+
 dst_bucket=${src_bucket/-input/-output}
-dst_prefix=${src_prefix}.bags
-dst_bag=${src_base}
+dst_prefix=${src_prefix_base}.bags
+dst_bag=${src_object_base}
 dst_object=${src_object}
 
 [ -d ${dst_bag} ] && rm -rf $dst_bag
 
 mkdir $dst_bag
 
-aws s3 cp $src_url ${dst_bag}/${dst_object} --no-progress || echo_exit "src cp failed"
-
-fits.sh -xc -i ${dst_bag} -o $dst_bag || echo_exit "fits.sh failed"
-
-aws s3api head-object --bucket ${src_bucket} --key ${src_prefix}.MODS/${src_base}.xml >/dev/null 2>&1
+# MODS
+aws s3api head-object --bucket ${src_bucket} --key ${src_prefix_base}.MODS/${src_object_base}.xml >/dev/null 2>&1
 
 if [ $? -eq 0 ]; then
-  aws s3 cp s3://${src_bucket}/${src_prefix}.MODS/${src_base}.xml ${dst_bag}/MODS.xml --no-progress || echo_exit "mods cp failed"
-
-	xmlstarlet val -w ${dst_bag}/MODS.xml
-
-	if [ $? -ne 0 ]; then
-    rm -rf $dst_bag
-		exit 1
-	fi
+  aws s3 cp s3://${src_bucket}/${src_prefix_base}.MODS/${src_object_base}.xml ${dst_bag}/MODS.xml --no-progress || echo_exit "mods cp failed"
+	xmlstarlet val -w ${dst_bag}/MODS.xml || echo_exit "xmlstarlet failed"
 fi
 
-bagit.py --processes 3 --md5 --sha256 --sha512 $dst_bag || echo_exit "bagit.py failed"
+# ASSETS
+for i in jpg tif; do
+	aws s3api head-object --bucket ${src_bucket} --key ${src_prefix_base}/${src_object_base}.$i >/dev/null 2>&1
 
+	if [ $? -eq 0 ]; then
+		aws s3 cp s3://${src_bucket}/${src_prefix_base}/${src_object_base}.$i ${dst_bag}/${src_object_base}.$i --no-progress || echo_exit "$i cp failed"
+		fits.sh -xc -i ${dst_bag}/${src_object_base}.$i -o $dst_bag/${src_object_base}.$i.fits.xml || echo_exit "fits.sh failed"
+	fi
+done
+
+bagit.py --processes 3 --md5 --sha256 --sha512 $dst_bag || echo_exit "bagit.py failed"
 aws s3 sync $dst_bag s3://${dst_bucket}/${dst_prefix}/${dst_bag} --no-progress || echo_exit "dst sync failed"
+
